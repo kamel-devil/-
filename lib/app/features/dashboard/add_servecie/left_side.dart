b8871dcf0f6f0ed43399ce5d95f5d59fcb8f4650
Kareem khaled <Kareemkhaled617@gmail.com>
2023-06-07 19:59:37 +0300

    update request

--- lib/app/features/dashboard/add_servecie/left_side.dart
+++ lib/app/features/dashboard/add_servecie/left_side.dart
@@ -29,6 +29,8 @@ class _CategoryLeftSideState extends State<CategoryLeftSide> {
   TextEditingController des = TextEditingController();
   String? supName;
 
+  GlobalKey<FormState> formKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -36,185 +38,199 @@ class _CategoryLeftSideState extends State<CategoryLeftSide> {
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Service",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w900,
-                  color: Colors.black,
-                ),
-              ),
-              const SizedBox(height: 12),
-              FutureBuilder(
-                  future: getSupplier(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      List sup = snapshot.data as List;
-
-                      return SizedBox(
-                        height: 60,
-                        child: DropdownSearch<String>(
-                          popupProps: const PopupProps.menu(
-                            showSelectedItems: true,
-                            showSearchBox: true,
-                            searchFieldProps:
-                                TextFieldProps(cursorColor: Colors.black),
-                            // disabledItemFn:
-                            //     (String s) =>
-                            //         s.startsWith('I'),
-                          ),
-                          items: List.generate(
-                              sup.length, (index) => sup[index]['name']),
-                          dropdownDecoratorProps: const DropDownDecoratorProps(
-                            baseStyle: TextStyle(
-                              color: Colors.black,
-                            ),
-                            dropdownSearchDecoration: InputDecoration(
-                                hintText: "Enter category",
-                                hintStyle: TextStyle(color: Colors.black)),
-                          ),
-                          onChanged: (v) {
-                            setState(() {
-                              supName = v;
-                              print(supName);
-                            });
-                          },
-                        ),
-                      );
-                    } else {
-                      return const CircularProgressIndicator();
-                    }
-                  }),
-              const SizedBox(height: 12),
-              TextFormField(
-                style: const TextStyle(color: Colors.black),
-                controller: service,
-                decoration: const InputDecoration(
-                    label: Text(
-                      "service Name",
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                    hintText: "Please write your service Name",
-                    hintStyle: TextStyle(
-                      color: Colors.black,
-                    )),
-              ),
-              const SizedBox(height: 12),
-              TextFormField(
-                style: const TextStyle(color: Colors.black),
-                controller: price,
-                decoration: const InputDecoration(
-                    label: Text(
-                      "price",
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                    hintText: "Please write your service price",
-                    hintStyle: TextStyle(
-                      color: Colors.black,
-                    )),
-              ),
-              const SizedBox(height: 12),
-              TextFormField(
-                style: const TextStyle(color: Colors.black),
-                keyboardType: TextInputType.multiline,
-                maxLines: 5,
-                // Handles Form Validation
-                validator: (value) {
-                  if (value == null || value.isEmpty || value.length > 200) {
-                    return 'Please describe service but keep it under 200 characters.';
-                  }
-                  return null;
-                },
-                controller: des,
-                textAlignVertical: TextAlignVertical.top,
-                decoration: const InputDecoration(
-                  alignLabelWithHint: true,
-                  hintMaxLines: 3,
-                  label: Text(
-                    "Description",
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                  hintText: 'Description service',
-                  hintStyle: TextStyle(
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Service",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w900,
                     color: Colors.black,
                   ),
                 ),
-              ),
-              const SizedBox(height: 12),
-              selctFile.isEmpty
-                  ? Container()
-                  : ClipRRect(
-                      borderRadius: BorderRadius.circular(16),
-                      child: Image.memory(
-                        selectedImageInBytes!,
-                        width: 150,
-                        height: 150,
+                const SizedBox(height: 12),
+                FutureBuilder(
+                    future: getSupplier(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        List sup = snapshot.data as List;
+
+                        return SizedBox(
+                          height: 60,
+                          child: DropdownSearch<String>(
+                            popupProps: const PopupProps.menu(
+                              showSelectedItems: true,
+                              showSearchBox: true,
+                              searchFieldProps:
+                                  TextFieldProps(cursorColor: Colors.black),
+                              // disabledItemFn:
+                              //     (String s) =>
+                              //         s.startsWith('I'),
+                            ),
+                            items: List.generate(
+                                sup.length, (index) => sup[index]['name']),
+                            dropdownDecoratorProps:
+                                const DropDownDecoratorProps(
+                              baseStyle: TextStyle(
+                                color: Colors.black,
+                              ),
+                              dropdownSearchDecoration: InputDecoration(
+                                  hintText: "Enter category",
+                                  hintStyle: TextStyle(color: Colors.black)),
+                            ),
+                            onChanged: (v) {
+                              setState(() {
+                                supName = v;
+                                print(supName);
+                              });
+                            },
+                          ),
+                        );
+                      } else {
+                        return const CircularProgressIndicator();
+                      }
+                    }),
+                const SizedBox(height: 12),
+                TextFormField(
+                  style: const TextStyle(color: Colors.black),
+                  controller: service,
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return 'Empty data';
+                    }
+                  },
+                  decoration: const InputDecoration(
+                      label: Text(
+                        "service Name",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      hintText: "Please write your service Name",
+                      hintStyle: TextStyle(
+                        color: Colors.black,
+                      )),
+                ),
+                const SizedBox(height: 12),
+                TextFormField(
+                  style: const TextStyle(color: Colors.black),
+                  controller: price,
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return 'Empty data';
+                    }
+                  },
+                  decoration: const InputDecoration(
+                      label: Text(
+                        "price",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      hintText: "Please write your service price",
+                      hintStyle: TextStyle(
+                        color: Colors.black,
+                      )),
+                ),
+                const SizedBox(height: 12),
+                TextFormField(
+                  style: const TextStyle(color: Colors.black),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 5,
+                  // Handles Form Validation
+                  validator: (value) {
+                    if (value == null || value.isEmpty || value.length > 200) {
+                      return 'Please describe service but keep it under 200 characters.';
+                    }
+                    return null;
+                  },
+                  controller: des,
+                  textAlignVertical: TextAlignVertical.top,
+                  decoration: const InputDecoration(
+                    alignLabelWithHint: true,
+                    hintMaxLines: 3,
+                    label: Text(
+                      "Description",
+                      style: TextStyle(
+                        color: Colors.black,
                       ),
                     ),
-              MaterialButton(
-                onPressed: () {
-                  selectFile(true);
-                },
-                height: 52,
-                elevation: 24,
-                color: const Color(0xFF03DAC5),
-                textColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(32)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(Icons.image),
-                    SizedBox(
-                      width: 8,
+                    hintText: 'Description service',
+                    hintStyle: TextStyle(
+                      color: Colors.black,
                     ),
-                    Text("Pick Image"),
-                  ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 24),
-              StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection('craftsman')
-                      .doc(FirebaseAuth.instance.currentUser!.uid)
-                      .snapshots(),
-                  builder: (context, AsyncSnapshot snapshot) {
-                    if (snapshot.hasData) {
-                      var data1 = snapshot.data;
-                      return data1['accept']
-                          ? MaterialButton(
-                              onPressed: () {
-                                print(supName);
-
-                                uploadFile();
-                                Navigator.pop(context);
-                              },
-                              minWidth: double.infinity,
-                              height: 52,
-                              elevation: 24,
-                              color: const Color(0xFF03DAC5),
-                              textColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(32)),
-                              child: const Text("Add"),
-                            )
-                          : Container();
-                    } else {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                  })
-            ],
+                const SizedBox(height: 12),
+                selctFile.isEmpty
+                    ? Container()
+                    : ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.memory(
+                          selectedImageInBytes!,
+                          width: 150,
+                          height: 150,
+                        ),
+                      ),
+                MaterialButton(
+                  onPressed: () {
+                    selectFile(true);
+                  },
+                  height: 52,
+                  elevation: 24,
+                  color: const Color(0xFF03DAC5),
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(Icons.image),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text("Pick Image"),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 24),
+                StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('craftsman')
+                        .doc(FirebaseAuth.instance.currentUser!.uid)
+                        .snapshots(),
+                    builder: (context, AsyncSnapshot snapshot) {
+                      if (snapshot.hasData) {
+                        var data1 = snapshot.data;
+                        return data1['accept']
+                            ? MaterialButton(
+                                onPressed: () {
+                                  if (formKey.currentState!.validate()) {
+                                    uploadFile();
+                                  }
+                                  Navigator.pop(context);
+                                },
+                                minWidth: double.infinity,
+                                height: 52,
+                                elevation: 24,
+                                color: const Color(0xFF03DAC5),
+                                textColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(32)),
+                                child: const Text("Add"),
+                              )
+                            : Container();
+                      } else {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                    })
+              ],
+            ),
           ),
         ),
       ),
